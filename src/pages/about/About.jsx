642d984655d1a8366d02ee0bca6ddf513c81e0fa
“Huriye_Dgit config --global user.email “demirogluhu@gmail.com <demirogluhu@gmail.com>
2022-07-19 10:22:33 -0700

    added

---
+++ src/pages/about/About.jsx
@@ -0,0 +1,33 @@
+
+import { AboutContainer, InfoContainer, StyledImage } from './About.style';
+
+import codingSvg from '../../assets/coding.svg';
+
+const About = () => {
+  return (
+    <AboutContainer wrap="wrap">
+      <StyledImage src={codingSvg} />
+      <div>
+        <h1>
+          About Software Developer <span>HuriyeDemiroglu </span>
+        </h1>
+      </div>
+      <InfoContainer>
+        <h2>Hi, I'am Huriye Demiroglu</h2>
+        <h3>I’m currently learning Full-Stack Development Languages.</h3>
+        <h4>
+          I've already known JS, ReactJS.
+        </h4>
+        <h2>
+         <a href="mailto:demirogluhu@gmail.com">Send email</a> :
+          demirogluhu@gmail.com
+        </h2>
+      </InfoContainer>
+    </AboutContainer>
+  );
+};
+
+export default About;
+
+
+
